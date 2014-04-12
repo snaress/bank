e04576ed0dfdbb95663920c22f9ace0337824350
kheops <jln.buisseret@gmail.com>
2014-04-12 23:01:27 +0200

    noiseAnimCurve: Update cmds
    Add amplitude bias evaluation
    Edit ui with noise options

--- tools/maya/anim/noiseAnimCurve/scripts/cmds.py
+++ tools/maya/anim/noiseAnimCurve/scripts/cmds.py
@@ -3,9 +3,14 @@ import maya.cmds as mc
 
 
 def getSelAnimCurves():
+    """ Get selcted curves from graphEditor
+        @return: (list) : Selected curves name """
     return mc.keyframe(q=True, n=True)
 
 def getCurveInfo(animCurves):
+    """ store info from selected curves
+        @param animCurves: (list) : Selected curves name
+        @return: (dict) : Selected curves info """
     curveInfo = {}
     for curve in animCurves:
         #-- Get Connections --#
@@ -25,21 +30,87 @@ def getCurveInfo(animCurves):
         curveInfo[curve]['duree'] = (nf[nk-1] - nf[0]) + 1
     return curveInfo
 
-def createRandomSeq(**kwargs):
+def getRandomSeq(**kwargs):
+    """ Create random sequence from params
+        @param kwargs: (dict) : Noise Params
+            @keyword noiseType: (str) : 'random' or sinRandom'
+            @keyword min: (float) : Amplitude Minimum
+            @keyword max: (float) : Amplitude Maximum
+            @keyword bias: (bool) : Amplitude Bias on or off
+            @keyword biasMin: (float) : Bias Minimum
+            @keyword biasMax: (float) : Bias Maximum
+            @keyword octaves: (int) : Number of random value to create
+            @keyword frequence: (int) : Octaves repetition
+        @return: (list) : Random sequence """
+    #-- Create Random Sequence --#
     randSeq = []
     for n in range(kwargs['octaves']):
-        randSeq.append(random.uniform(kwargs['min'], kwargs['max']))
+        rand = random.uniform(kwargs['min'], kwargs['max'])
+        if kwargs['bias']:
+            if not rand > kwargs['biasMax'] and not rand < kwargs['biasMin']:
+                if rand > (kwargs['min'] + kwargs['max'])/2:
+                    rand = random.uniform(kwargs['biasMax'], kwargs['max'])
+                else:
+                    rand = random.uniform(kwargs['biasMin'], kwargs['min'])
+        randSeq.append(rand)
+    #-- Create Random Frequence --#
     rOctaves = randSeq
     for m in range(kwargs['frequence']-1):
         randSeq.extend(rOctaves)
+    #-- Return Random List --#
+    printNoiseParams(**kwargs)
     return randSeq
 
 def createCurves(curveInfo, randSeq):
-    print curveInfo
+    """ Create new curves with noise params
+        @param curveInfo: (dict) : Selected curves info
+        @param randSeq: (list) : Random sequence
+        @return: (dict) : New curves info """
+    newCurves = {}
     for curve in curveInfo.keys():
-        step = curveInfo[curve]['duree'] / len(randSeq)
+        newCurves[curve] = {}
+        step = curveInfo[curve]['duree'] / len(randSeq)-1
+        newCurves[curve]['step'] = step
         for n, rand in enumerate(randSeq):
-            ind = curveInfo[curve]['nf'][0] + (step * n)
+            ind = curveInfo[curve]['nf'][0] + (step * (n+1))
             val = mc.keyframe(curve, q=True, t=(ind, ind), ev=True)
             if val is not None:
-                mc.setKeyframe(curve, t=[ind], v=val[0]+rand)
+                newCurves[curve][ind] = {}
+                newCurves[curve][ind]['rand'] = rand
+                newCurves[curve][ind]['val'] = val[0]+rand
+    return newCurves
+
+def applyOnCurves(newCurves):
+    """ Apply new curves params on selected curves
+        @param newCurves: (dict) : New curves info """
+    printNewCurvesInfo(newCurves)
+    for curve in newCurves.keys():
+        for k, v in newCurves[curve].iteritems():
+            if not k == 'step':
+                mc.setKeyframe(curve, t=k, v=v['val'])
+
+def printNoiseParams(**kwargs):
+    """ Print noise params
+        @param kwargs: (dict) : Noise Params """
+    print "\n", "#" * 60
+    print "#-- Noise Params --#"
+    for k, v in kwargs.iteritems():
+        print k, ' = ', v
+    print "#" * 60
+
+def printNewCurvesInfo(newCurves):
+    """ Print new curves info
+        @param newCurves: (dict) : New curves info """
+    print "\n", "#" * 60
+    print "#-- New Curves Info --#"
+    for curve in newCurves.keys():
+        print "%s :" % curve
+        print "\tStep = %s" % newCurves[curve]['step']
+        frames = []
+        for ind in newCurves[curve].keys():
+            if not ind == 'step':
+                frames.append(ind)
+        frames.sort()
+        for f in frames:
+            print "\tFrame %s = %s" % (f, newCurves[curve][f])
+    print "#" * 60
--- tools/maya/anim/noiseAnimCurve/scripts/noiseAnimCurveUi.py
+++ tools/maya/anim/noiseAnimCurve/scripts/noiseAnimCurveUi.py
@@ -17,25 +17,52 @@ class NoiseAnimCurveUi(noiseAnimCurveClass, noiseAnimCurveUiClass):
     def _setupUi(self):
         """ Setup main window """
         self.setupUi(self)
+        self.miToolTips.triggered.connect(self.on_toolTips)
         self.bNoise.clicked.connect(self.noiseCurve)
 
     def noiseCurve(self):
+        """ Command launch when bNoise is clicked """
         curves = self.cmds.getSelAnimCurves()
         curveInfo = self.cmds.getCurveInfo(curves)
-        randSeq = self.cmds.createRandomSeq(**self.getNoiseParams)
-        self.cmds.createCurves(curveInfo, randSeq)
+        randSeq = self.cmds.getRandomSeq(**self.getNoiseParams)
+        newCurves = self.cmds.createCurves(curveInfo, randSeq)
+        self.cmds.applyOnCurves(newCurves)
 
     @property
     def getNoiseParams(self):
+        """ Get noise params from ui
+            @return: (dict) : Noise params """
         params = {}
+        if self.cbRandom.isChecked():
+            params['noiseType'] = 'random'
+        elif self.cbSinRandom.isChecked():
+            params['noiseType'] = 'sinRandom'
         params['min'] = self.sbAmpMin.value()
         params['max'] = self.sbAmpMax.value()
-        params['frequence'] = self.sbFreq.value()
+        params['bias'] = self.gbBias.isChecked()
+        params['biasMin'] = self.sbBiasMin.value()
+        params['biasMax'] = self.sbBiasMax.value()
         params['octaves'] = self.sbOctaves.value()
-        params['Mmin'] = self.sbMaxMin.value()
-        params['mMax'] = self.sbMinMax.value()
-        params['KeepKeys'] = self.cbKeepKeys.isChecked()
+        params['frequence'] = self.sbFreq.value()
         return params
+
+    def on_toolTips(self):
+        """ Command launch when help menu action 'Tool Tips' is triggered """
+        for k, v in self.toolTips.iteritems():
+            if self.miToolTips.isChecked():
+                exec("self.%s.setToolTip(%r)" % (k, v))
+            else:
+                exec("self.%s.setToolTip('')" % k)
+
+    @property
+    def toolTips(self):
+        """ Store tool tips in dict
+            @return: (dict) : Tool tips str """
+        toolTipDict = {}
+        toolTipDict['lNoiseType'] = "Noise type choice for random sequence creation"
+        toolTipDict['cbRandom'] = "Uniform random"
+        toolTipDict['cbSinRandom'] = "Sinusoidal random"
+        return toolTipDict
 
 
 if __name__ == '__main__':
