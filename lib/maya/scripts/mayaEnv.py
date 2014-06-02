2bc140b163ac0c5d28b7bccb718d14e9ecd4ea7a
kheops <jln.buisseret@gmail.com>
2014-06-02 20:18:40 +0200

    lib.system: Add dictToList() function

--- lib/maya/scripts/mayaEnv.py
+++ lib/maya/scripts/mayaEnv.py
@@ -1,14 +1,15 @@
-import os
-import sys
+import os, sys
 
 
 def editSysPath():
-    print "#-- Check SysPath --#"
+    print "\n#-- Check SysPath --#"
     libPath = os.path.join('F:', os.sep, 'rnd', 'lib')
     bankPath = os.path.join('F:', os.sep, 'rnd', 'workspace', 'bank')
     for path in [libPath, bankPath]:
         if not path in sys.path:
-            print "Add %s in sysPath ..." % path
+            print "Add %s to sysPath ..." % path
             sys.path.insert(0, path)
 
-editSysPath()
+
+if __name__ == '__main__':
+    editSysPath()
