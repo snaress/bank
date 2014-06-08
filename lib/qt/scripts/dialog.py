c60eeeb49aebcab22ee339a58f4540d36c5b79c8
kheops <jln.buisseret@gmail.com>
2014-06-08 14:20:35 +0200

    lib.qt.fileDialog: Add fileMode options

--- lib/qt/scripts/dialog.py
+++ lib/qt/scripts/dialog.py
@@ -2,19 +2,32 @@ from PyQt4 import QtGui, QtCore, uic
 from lib import qt
 
 
-def fileDialog(fdMode='open', fdRoot=None, fdRoots=None, fdFilters=None, fdCmd=None):
+def fileDialog(fdMode='open', fdFileMode='AnyFile', fdRoot=None, fdRoots=None,
+               fdFilters=None, fdCmd=None):
     """ FileDialog popup
         @param fdMode: (str) : setAcceptMode 'open' or 'save'
+        @param fdFileMode: (str) : setFileMode 'AnyFile', 'ExistingFile', 'Directory', 'DirectoryOnly'
         @param fdRoot: (str) : Start root path
         @param fdRoots: (list) : List of recent files (list[str(QUrl)])
         @param fdFilters: (list) : List of extensions
         @param fdCmd: (object) : Command for accepted execution
         @return: (object) : QFileDiaolog widget object """
     fd = QtGui.QFileDialog()
+    #-- FileDialog AcceptedMode --#
     if fdMode == 'open':
         fd.setAcceptMode(QtGui.QFileDialog.AcceptOpen)
     elif fdMode == 'save':
         fd.setAcceptMode(QtGui.QFileDialog.AcceptSave)
+    #-- FileDialog FileMode --#
+    if fdFileMode == 'AnyFile':
+        fd.setFileMode(QtGui.QFileDialog.AnyFile)
+    elif fdFileMode == 'ExistingFile':
+        fd.setFileMode(QtGui.QFileDialog.ExistingFile)
+    elif fdFileMode == 'Directory':
+        fd.setFileMode(QtGui.QFileDialog.Directory)
+    elif fdFileMode == 'DirectoryOnly':
+        fd.setFileMode(QtGui.QFileDialog.DirectoryOnly)
+    #-- FileDialog Params --#
     if fdRoot is not None:
         fd.setDirectory(fdRoot)
     if fdRoots is not None:
