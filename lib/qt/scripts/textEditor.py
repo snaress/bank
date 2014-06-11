3385d91005865fe51f415729ee1ca292216de237
kheops <jln.buisseret@gmail.com>
2014-06-11 21:53:50 +0200

    lib.qt: refonte du text editor

--- lib/qt/scripts/textEditor.py
+++ lib/qt/scripts/textEditor.py
@@ -1,116 +1,122 @@
-import sys, os
+import os, sys
 from lib import qt
 from lib.qt.scripts import dialog2
 from PyQt4 import QtGui, QtCore, uic
 from lib.system.scripts import procFile as pFile
 
 
-textEditorClass, textEditorUiClass = uic.loadUiType(qt.uiList['textEditorWidget'])
-class TextEditorWidget(textEditorClass, textEditorUiClass):
+textEditorClass, textEditorUiClass = uic.loadUiType(qt.uiList['textEditor'])
+class TextEditor(textEditorClass, textEditorUiClass):
 
     def __init__(self):
+        super(TextEditor, self).__init__()
         self.iconDir = os.path.join(qt.toolPath, '_lib', 'ima', 'textEditor')
-        super(TextEditorWidget, self).__init__()
+        self.sizeList = [6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,32,36,40,44,48,
+                         54,60,66,72,80,88,96]
         self._setupUi()
 
     def _setupUi(self):
-        """ Setup Widget """
         self.setupUi(self)
         self._setupToolBarUp()
         self._setupToolBarDn()
 
     def _setupToolBarUp(self):
-        """ Setup widget toolBar up """
-        #-- Clear --#
-        self.bClearText.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'textClear.png')))
-        self.bClearText.clicked.connect(self.on_clearText)
-        self.bClearText.setToolTip("Clear text")
+        #-- Clear Text --#
+        self.bClearText = self.newToolBarBtn(os.path.join(self.iconDir, 'textClear.png'),
+                                             self.on_clearText, "Clear text")
+        self.toolBarUp.addWidget(self.bClearText)
         #-- Load File --#
-        self.bLoadFile.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fileLoad.png')))
-        self.bLoadFile.clicked.connect(self.on_loadFile)
-        self.bLoadFile.setToolTip("Load file")
+        self.bLoadFile = self.newToolBarBtn(os.path.join(self.iconDir, 'fileLoad.png'),
+                                            self.on_loadFile, "Load file")
+        self.toolBarUp.addWidget(self.bLoadFile)
         #-- Save File --#
-        self.bSaveFile.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fileSave.png')))
-        self.bSaveFile.clicked.connect(self.on_saveFile)
-        self.bSaveFile.setToolTip("Save file")
+        self.bSaveFile = self.newToolBarBtn(os.path.join(self.iconDir, 'fileSave.png'),
+                                            self.on_saveFile, "Save file")
+        self.toolBarUp.addWidget(self.bSaveFile)
         #-- Copy Text --#
-        self.bTextCopy.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'textCopy.png')))
-        self.bTextCopy.clicked.connect(self.on_textCopy)
-        self.bTextCopy.setToolTip("Copy text")
+        self.toolBarUp.addSeparator()
+        self.bTextCopy = self.newToolBarBtn(os.path.join(self.iconDir, 'textCopy.png'),
+                                            self.on_textCopy, "Copy text")
+        self.toolBarUp.addWidget(self.bTextCopy)
         #-- Cut Text --#
-        self.bTextCut.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'textCut.png')))
-        self.bTextCut.clicked.connect(self.on_textCut)
-        self.bTextCut.setToolTip("Cut text")
+        self.bTextCut = self.newToolBarBtn(os.path.join(self.iconDir, 'textCut.png'),
+                                           self.on_textCut, "Cut text")
+        self.toolBarUp.addWidget(self.bTextCut)
         #-- Paste Text --#
-        self.bTextPaste.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'textPaste.png')))
-        self.bTextPaste.clicked.connect(self.on_textPaste)
-        self.bTextPaste.setToolTip("Paste text")
+        self.bTextPaste = self.newToolBarBtn(os.path.join(self.iconDir, 'textPaste.png'),
+                                             self.on_textPaste, "Paste text")
+        self.toolBarUp.addWidget(self.bTextPaste)
         #-- Undo --#
-        self.bUndo.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'undo.png')))
-        self.bUndo.clicked.connect(self.on_undo)
-        self.bUndo.setToolTip("Undo")
+        self.toolBarUp.addSeparator()
+        self.bUndo = self.newToolBarBtn(os.path.join(self.iconDir, 'undo.png'), self.on_undo, "Undo")
+        self.toolBarUp.addWidget(self.bUndo)
         #-- Redo --#
-        self.bRedo.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'redo.png')))
-        self.bRedo.clicked.connect(self.on_undo)
-        self.bRedo.setToolTip("Redo")
+        self.bRedo = self.newToolBarBtn(os.path.join(self.iconDir, 'redo.png'), self.on_undo, "Redo")
+        self.toolBarUp.addWidget(self.bRedo)
 
+    # noinspection PyUnresolvedReferences
     def _setupToolBarDn(self):
-        """ Setup widget toolBar down """
-        #-- Font Size --#
+        #-- Font Style --#
+        self.cbFontStyle = QtGui.QFontComboBox()
+        self.cbFontStyle.setMaximumWidth(130)
         self.cbFontStyle.currentFontChanged.connect(self.on_fontStyle)
-        sizeList = [6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,32,36,40,44,48,54,60,66,72,80,88,96]
-        self.cbFontSize.addItems([str(s) for s in sizeList])
+        self.cbFontStyle.setToolTip("Font style")
+        self.toolBarDn.addWidget(self.cbFontStyle)
+        #-- Font Size --#
+        self.cbFontSize = QtGui.QComboBox()
+        self.cbFontSize.addItems([str(s) for s in self.sizeList])
         self.cbFontSize.setCurrentIndex(2)
         self.cbFontSize.currentIndexChanged.connect(self.on_fontSize)
         self.cbFontSize.setToolTip("Font size")
+        self.toolBarDn.addWidget(self.cbFontSize)
         #-- Font Color --#
-        self.bFontColor.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fontColor.png')))
-        self.bFontColor.clicked.connect(self.on_fontColor)
-        self.bFontColor.setToolTip("Font color")
+        self.toolBarDn.addSeparator()
+        self.bFontColor = self.newToolBarBtn(os.path.join(self.iconDir, 'fontColor.png'),
+                                             self.on_fontColor, "Font color")
+        self.toolBarDn.addWidget(self.bFontColor)
         #-- Font Bg Color --#
-        self.bFontBgColor.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fontBgColor.png')))
-        self.bFontBgColor.clicked.connect(self.on_fontBgColor)
-        self.bFontBgColor.setToolTip("Font bg color")
+        self.bFontBgColor = self.newToolBarBtn(os.path.join(self.iconDir, 'fontBgColor.png'),
+                                               self.on_fontBgColor, "Font bg color")
+        self.toolBarDn.addWidget(self.bFontBgColor)
         #-- Font Bold --#
-        self.bFontBold.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fontBold.png')))
-        self.bFontBold.clicked.connect(self.on_fontBold)
-        self.bFontBold.setToolTip("Bold")
+        self.toolBarDn.addSeparator()
+        self.bFontBold = self.newToolBarBtn(os.path.join(self.iconDir, 'fontBold.png'),
+                                            self.on_fontBold, "Bold")
+        self.toolBarDn.addWidget(self.bFontBold)
         #-- Font Italic --#
-        self.bFontItalic.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fontItalic.png')))
-        self.bFontItalic.clicked.connect(self.on_fontItalic)
-        self.bFontItalic.setToolTip("Italic")
+        self.bFontItalic = self.newToolBarBtn(os.path.join(self.iconDir, 'fontItalic.png'),
+                                              self.on_fontItalic, "Italic")
+        self.toolBarDn.addWidget(self.bFontItalic)
         #-- Font Underline --#
-        self.bFontUnderline.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'fontUnderline.png')))
-        self.bFontUnderline.clicked.connect(self.on_fontUnderline)
-        self.bFontUnderline.setToolTip("Underline")
+        self.bFontUnderline = self.newToolBarBtn(os.path.join(self.iconDir, 'fontUnderline.png'),
+                                                 self.on_fontUnderline, "Underline")
+        self.toolBarDn.addWidget(self.bFontUnderline)
         #-- Align Left --#
-        self.bAlignLeft.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'alignLeft.png')))
-        self.bAlignLeft.clicked.connect(self.on_alignLeft)
-        self.bAlignLeft.setToolTip("Align left")
+        self.toolBarDn.addSeparator()
+        self.bAlignLeft = self.newToolBarBtn(os.path.join(self.iconDir, 'alignLeft.png'),
+                                             self.on_alignLeft, "Align left")
+        self.toolBarDn.addWidget(self.bAlignLeft)
         #-- Align Center --#
-        self.bAlignCenter.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'alignCenter.png')))
-        self.bAlignCenter.clicked.connect(self.on_alignCenter)
-        self.bAlignCenter.setToolTip("Align center")
+        self.bAlignCenter = self.newToolBarBtn(os.path.join(self.iconDir, 'alignCenter.png'),
+                                               self.on_alignCenter, "Align center")
+        self.toolBarDn.addWidget(self.bAlignCenter)
         #-- Align Right --#
-        self.bAlignRight.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'alignRight.png')))
-        self.bAlignRight.clicked.connect(self.on_alignRight)
-        self.bAlignRight.setToolTip("Align right")
+        self.bAlignRight = self.newToolBarBtn(os.path.join(self.iconDir, 'alignRight.png'),
+                                              self.on_alignRight, "Align right")
+        self.toolBarDn.addWidget(self.bAlignRight)
         #-- Align Justify --#
-        self.bAlignJustify.setIcon(QtGui.QIcon(os.path.join(self.iconDir, 'alignJustify.png')))
-        self.bAlignJustify.clicked.connect(self.on_alignJustify)
-        self.bAlignJustify.setToolTip("Align justify")
+        self.bAlignJustify = self.newToolBarBtn(os.path.join(self.iconDir, 'alignJustify.png'),
+                                                self.on_alignJustify, "Align justify")
+        self.toolBarDn.addWidget(self.bAlignJustify)
 
     def on_clearText(self):
-        """ Clear text cmd """
         self.teText.clear()
 
     def on_loadFile(self):
-        """ Launch the load fileDialog """
         self.fdLoad = dialog2.fileDialog(fdMode='open', fdFileMode='AnyFile', fdCmd=self.fd_loadAccept)
         self.fdLoad.exec_()
 
     def fd_loadAccept(self):
-        """ Load text from given file """
         fileIn = self.fdLoad.selectedFiles()
         if fileIn and not str(fileIn[0]) in ['', ' ']:
             fileName = str(fileIn[0]).split(os.sep)[-1]
@@ -121,12 +127,10 @@ class TextEditorWidget(textEditorClass, textEditorUiClass):
                 self.teText.setText(''.join(text))
 
     def on_saveFile(self):
-        """ Launch the save fileDialog """
         self.fdSave = dialog2.fileDialog(fdMode='save', fdFileMode='AnyFile', fdCmd=self.fd_saveAccept)
         self.fdSave.exec_()
 
     def fd_saveAccept(self):
-        """ Save text to given file (default extension is html) """
         fileOut = self.fdSave.selectedFiles()
         if fileOut and not str(fileOut[0]) in ['', ' ']:
             fileName = str(fileOut[0]).split(os.sep)[-1]
@@ -142,23 +146,18 @@ class TextEditorWidget(textEditorClass, textEditorUiClass):
                 raise IOError("Can not write file !!!")
 
     def on_textCopy(self):
-        """ Copy text cmd """
         self.teText.copy()
 
     def on_textCut(self):
-        """ Cut text cmd """
         self.teText.cut()
 
     def on_textPaste(self):
-        """ Paste text cmd """
         self.teText.paste()
 
     def on_undo(self):
-        """ Undo cmd """
         self.teText.undo()
 
     def on_redo(self):
-        """ Undo cmd """
         self.teText.redo()
 
     def on_fontStyle(self):
@@ -166,57 +165,84 @@ class TextEditorWidget(textEditorClass, textEditorUiClass):
         self.teText.setCurrentFont(font)
 
     def on_fontSize(self):
-        """ Change font size cmd """
         size = int(self.cbFontSize.itemText(self.cbFontSize.currentIndex()))
         self.teText.setFontPointSize(size)
 
     # noinspection PyArgumentList
     def on_fontColor(self):
-        """ Change font color cmd """
         color = QtGui.QColorDialog.getColor()
         self.teText.setTextColor(color)
 
     # noinspection PyArgumentList
     def on_fontBgColor(self):
-        """ Change font background color cmd """
         color = QtGui.QColorDialog.getColor()
         self.teText.setTextBackgroundColor(color)
 
     def on_fontBold(self):
-        """ Change font to bold cmd """
         if self.teText.fontWeight() == 50:
             self.teText.setFontWeight(QtGui.QFont.Bold)
         else:
             self.teText.setFontWeight(QtGui.QFont.Normal)
 
     def on_fontItalic(self):
-        """ Change font to italic cmd """
         self.teText.setFontItalic(not self.teText.fontItalic())
 
     def on_fontUnderline(self):
-        """ Change font to underline cmd """
         self.teText.setFontUnderline(not self.teText.fontUnderline())
 
     def on_alignLeft(self):
-        """ Align text left side """
         self.teText.setAlignment(QtCore.Qt.AlignLeft)
 
     def on_alignCenter(self):
-        """ Align text center """
         self.teText.setAlignment(QtCore.Qt.AlignCenter)
 
     def on_alignRight(self):
-        """ Align text right side """
         self.teText.setAlignment(QtCore.Qt.AlignRight)
 
     def on_alignJustify(self):
-        """ Align text justify """
         self.teText.setAlignment(QtCore.Qt.AlignJustify)
 
+    @staticmethod
+    def newToolBarBtn(iconPath, cmd, toolTip):
+        """ Create new toolBar action
+            @param iconPath: (str) : Icon absolut path
+            @param cmd: (object) : Command to connect
+            @param toolTip: (str) : Tool tip
+            @return: (object) : New toolBar button """
+        newBtn = QtGui.QPushButton()
+        newBtn.setIcon(QtGui.QIcon(iconPath))
+        newBtn.clicked.connect(cmd)
+        newBtn.setToolTip(toolTip)
+        return newBtn
+
+    @property
+    def fileActionGrp(self):
+        """ Get Actions of the group 'File'
+            @return: (list) : List of file actions """
+        return [self.bClearText, self.bLoadFile, self.bSaveFile]
+
+    @property
+    def editActionGrp(self):
+        """ Get Actions of the group 'Edit'
+            @return: (list) : List of edit actions """
+        return [self.bTextCopy, self.bTextCut, self.bTextPaste, self.bUndo, self.bRedo]
+
+    @property
+    def textActionGrp(self):
+        """ Get Actions of the group 'Text'
+            @return: (list) : List of text actions """
+        return [self.bAlignLeft, self.bAlignCenter, self.bAlignRight, self.bAlignJustify]
+
+    @property
+    def fontActionGrp(self):
+        """ Get Actions of the group 'Font'
+            @return: (list) : List of font actions """
+        return [self.cbFontStyle, self.cbFontSize, self.bFontColor, self.bFontBgColor,
+                self.bFontBold, self.bFontItalic, self.bFontUnderline]
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    window = TextEditorWidget()
+    window = TextEditor()
     window.show()
     sys.exit(app.exec_())
