9b608fec2f26801094755badca0ea06f30093415
kheops <jln.buisseret@gmail.com>
2014-08-11 13:23:04 +0200

    lib.maya.procMaya: Add importScene() , saveSceneAs()

--- lib/maya/scripts/procMaya.py
+++ lib/maya/scripts/procMaya.py
@@ -23,3 +23,23 @@ def loadScene(sceneName, force=True):
         @param force: (bool) : Force opening """
     print "Opening Maya Scene: %s" % sceneName
     mc.file(sceneName, o=True, f=force)
+
+def importScene(sceneName, force=True):
+    """ Import given scene
+        @param sceneName: (str) : Scene absolut path
+        @param force: (bool) : Force opening """
+    print "Importing Maya Scene: %s" % sceneName
+    mc.file(sceneName, i=True, type="mayaAscii", pr=True, lrd="all", f=force)
+
+def saveSceneAs(sceneName, force=True, keepCurrentName=False):
+    """ Save scene with given name
+        @param sceneName: (str) : Scene absolut path
+        @param force: (bool) : Force opening
+        @param keepCurrentName: (bool) : Keep original scene name """
+    print "Saving Maya Scene: %s" % sceneName
+    currentSceneName = mc.file(q=True, sn=True)
+    mc.file(rn=sceneName)
+    mc.file(s=True, type="mayaAscii", f=force)
+    if keepCurrentName:
+        print "Keep Scene Name: %s" % currentSceneName
+        mc.file(rn=currentSceneName)
