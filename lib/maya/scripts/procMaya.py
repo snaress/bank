f815f1dbdd7a3aa75c6e855ec8cd584f16460fd1
kheops <jln.buisseret@gmail.com>
2014-08-21 01:50:13 +0200

    move loadMentalRay() to tools.maya.render.proc.scripts.procRender

--- lib/maya/scripts/procMaya.py
+++ lib/maya/scripts/procMaya.py
@@ -4,19 +4,6 @@ except:
     pass
 
 
-def loadMentalRay(currentEngine=True):
-    """ Load Mental Ray plugin
-        @param currentEngine: (bool) : Set MentalRay as current render engine """
-    name = "Mayatomr"
-    if not mc.pluginInfo(name, q=True, loaded=True):
-        print "Load MentalRayPlugin() ..."
-        mc.loadPlugin(name)
-        mc.pluginInfo(name, edit=True, autoload=True)
-    if currentEngine:
-        print "Set MentalRay as current render engine ..."
-        mc.setAttr('defaultRenderGlobals.currentRenderer', 'mentalRay', type='string')
-    print "# Result: mental ray Plugin loaded #"
-
 def loadScene(sceneName, force=True):
     """ Open given scene
         @param sceneName: (str) : Scene absolut path
