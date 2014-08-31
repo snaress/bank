e59dfd31b4dbec7fb23315583f68d52ccdd7c695
kheops <jln.buisseret@gmail.com>
2014-09-01 00:34:37 +0200

    add --open option

--- appli/grapher/__exe__.py
+++ appli/grapher/__exe__.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, optparse
 
 
 #-- SysPath --#
@@ -6,9 +6,21 @@ wsPath = os.path.join('F:', os.sep, 'rnd', 'workspace')
 bankPath = os.path.join(wsPath, 'bank')
 sys.path.insert(0, bankPath)
 
+#-- Options --#
+usage = "Grapher launcher usage ..."
+parser = optparse.OptionParser(usage=usage)
 
-#-- Launch Tool --#
-from appli.grapher.scripts import grapherUi
+parser.add_option('-o', '--open', type='string', help="[str] Open given graphFile.")
+
+
 if __name__ == '__main__':
-    grapherUi.launch()
+    options, args = parser.parse_args()
+    options = eval(str(options))
 
+    #-- Launch Tool --#
+    from appli.grapher.scripts import grapherUi
+    if options['open'] is None:
+        grapherUi.launch()
+    else:
+        print options['open']
+        grapherUi.launch(graph=options['open'])
