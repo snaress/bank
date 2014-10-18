16e702c8d71aaf535fa69ec888f7244b41f13ead
kheops <jln.buisseret@gmail.com>
2014-10-18 04:59:44 +0200

    lib.qt.preview: First commit

--- lib/qt/__init__.py
+++ lib/qt/__init__.py
@@ -6,6 +6,11 @@ toolPath = os.path.normpath(os.path.dirname(__file__))
 toolName = toolPath.split(os.sep)[-1]
 toolPack = __package__
 
+
+#-- Global Var --#
+libPath = os.path.join(toolPath, '_lib')
+
+
 #-- UI Files --#
 uiL = os.listdir(os.path.join(toolPath, 'ui')) or []
 uiList = {}
